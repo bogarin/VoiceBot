528edfe125f38eaeac945ae6e4ecade9b81954cc
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-03-16 02:56:03 +0400

    Add a Discord client wrapper.

---
+++ src/Discord/BotClient.ts
@@ -0,0 +1,55 @@
+import Discord, { Guild, Message } from "discord.js";
+import config from "../config";
+import logger from "../utils/logger";
+
+class BotClient extends Discord.Client {
+  public messageEventHandler: (message: Message) => Promise<void>;
+  constructor(token: string = config.DISCORD_BOT_TOKEN) {
+    super();
+
+    this.token = token;
+
+    this.on("ready", this.onReady);
+    this.on("guildCreate", this.onGuildCreate);
+    this.on("guildDelete", this.onGuildDelete);
+    this.on("message", this.onMessage);
+    this.on("error", this.onError);
+
+    this.login(token);
+  }
+
+  // Event handlers
+  public onError = (error: Error) => {
+    logger.error(`BOT_CLIENT: Discord bot error: ${error.name}:${error.message}.`);
+  };
+
+  public onReady = () => {
+    logger.info(
+      `BOT_CLIENT: Bot has started, with ${this.users.size} users, in ${this.channels.size} channels of ${
+        this.guilds.size
+      } guilds.`
+    );
+  };
+
+  public onGuildCreate = (guild: Guild) => {
+    logger.info(
+      `BOT_CLIENT: Added to guild: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members.`
+    );
+  };
+
+  public onGuildDelete = (guild: Guild) => {
+    logger.info(`BOT_CLIENT: Removed from guild: ${guild.name} (id: ${guild.id})`);
+  };
+
+  public onMessage = async (message: Message) => {
+    // Ignore non-guild messages
+    if (!message.guild) return;
+
+    // Ignore bots
+    if (message.member.user.bot) return;
+
+    if (this.messageEventHandler) this.messageEventHandler(message);
+  };
+}
+
+export default BotClient;

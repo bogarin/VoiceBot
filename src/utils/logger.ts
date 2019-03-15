ce97caac1b51b5678f1636e9f6ce9e27ba2d94a7
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-03-16 02:55:46 +0400

    Add utils.

---
+++ src/utils/logger.ts
@@ -0,0 +1,26 @@
+import winston, { format } from "winston";
+const colorizer = winston.format.colorize();
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" })
+  ]
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: format.combine(
+        format.timestamp(),
+        format.simple(),
+        format.printf(info => `${colorizer.colorize(info.level, `${info.timestamp} - ${info.level}: `)}${info.message}`)
+      )
+    })
+  );
+}
+
+export default logger;

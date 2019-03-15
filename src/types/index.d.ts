3d070c60a4c5aa2b61d69f6542ac29d71908e084
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-03-16 02:57:02 +0400

    Add types.

---
+++ src/types/index.d.ts
@@ -0,0 +1,17 @@
+import { GuildMember, Message, User } from "discord.js";
+
+export type CommandTaskFn = (params: string, member: GuildMember, message?: Message, user?: User) => Promise<void>;
+
+export interface ICommandTaskMapping {
+  [s: string]: CommandTaskFn;
+}
+
+export type onEvent = (...args: any[]) => void;
+
+export interface IEventMapping {
+  [s: string]: onEvent;
+}
+
+export interface IEventClassMapping {
+  [s: string]: IEventMapping;
+}

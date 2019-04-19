51437b2befbc19ed010c9a8aef295d1157888562
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-04-20 00:07:15 +0400

    Implement a search based play command.

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -1,5 +1,7 @@
 import { GuildMember, Message, User } from "discord.js";
+import youtubeSearch from "youtube-search";
 import ytdl from "ytdl-core";
+import config from "../config";
 import BotClient from "../Discord/BotClient";
 import { ICommandTaskMapping, IEventClassMapping } from "../types";
 import logger from "../utils/logger";
@@ -53,6 +55,55 @@ export default class CommandRunner {
     logger.info(`COMMAND_RUNNER: Playing ${params} on VC:${member.voice.channel.name} as per command.`);
   };
 
+  public playCommand = async (params: string, member: GuildMember) => {
+    if (!member.voice.channel) return;
+
+    const connection = await member.voice.channel.join();
+
+    // Search for a clip to play
+    const searchOptions: youtubeSearch.YouTubeSearchOptions = {
+      maxResults: 1,
+      key: config.YOUTUBE_API_KEY
+    };
+
+    const searchResponse: youtubeSearch.YouTubeSearchResults[] = await new Promise((resolve, reject) => {
+      youtubeSearch(params, searchOptions, (err, res) => {
+        if (err) {
+          reject(err);
+          logger.error(
+            `COMMAND_RUNNER: Failed to find an applicable clip for ${params} as per the request of ${
+              member.user.username
+            } from ${member.voice.channel.name}.`
+          );
+          return;
+        }
+        resolve(res);
+      });
+    });
+
+    const url = searchResponse[0] ? searchResponse[0].link : null;
+
+    if (!url) {
+      logger.error(`COMMAND_RUNNER: No valid Youtube URL found to play.`);
+      return;
+    }
+
+    // Play the clip
+    logger.info(`Starting to play ${url} on ${member.voice.channel.name}.`);
+    const dispatcher = connection.play(
+      ytdl(url, {
+        quality: "highestaudio"
+      })
+    );
+    dispatcher.on("error", () => {
+      logger.error(`COMMAND_RUNNER: Error while playing ${url} on ${member.voice.channel.name}.`);
+    });
+    dispatcher.on("finish", () => {
+      logger.info(`COMMAND_RUNNER: Finished playing ${url} on ${member.voice.channel.name}.`);
+    });
+    logger.info(`COMMAND_RUNNER: Playing ${url} on VC:${member.voice.channel.name} as per command.`);
+  };
+
   public pingCommand = async (params: string, member: GuildMember, message: Message) => {
     const botMessage = (await message.channel.send("Ping?")) as Message;
     await botMessage.edit(`Pong! Latency is ${botMessage.createdTimestamp - message.createdTimestamp}ms.`);
@@ -67,6 +118,7 @@ export default class CommandRunner {
       join: this.joinCommand,
       leave: this.leaveCommand,
       playurl: this.playURLCommand,
+      play: this.playCommand,
       ping: this.pingCommand
     };
 
